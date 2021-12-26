d82cb5ba8731d8fa7af217587e058725d1689daf
Flow-matic <jonathonvinson22@gmail.com>
2021-12-26 23:38:50 +0000

    Add check winner if else won

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,8 @@ const resetDiv = document.querySelector('.reset');
 const cellDiv = document.querySelectorAll('.game-cell');
 
 // game constants
-const xSymbol = 'x';
-continue oSymbol = 'o';
+const xSymbol = '×';
+const oSymbol = '○';
 
 // game variables
 let gameIsLive = true;
@@ -33,40 +33,26 @@ const handleWin = (letter) => {
       const middleRight = cellDivs[5].classList[1];
       const bottomLeft = cellDivs[6].classList[1];
       const bottomMiddle = cellDivs[7].classList[1];
-      const BottomRight = cellDivs[8].classList[1];
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      const bottomRight = cellDivs[8].classList[1];
 
 // check winner
+if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+    handleWin(topLeft);
+    cellDiv[0].classList.add('won');
+    cellDiv[1].classList.add('won');
+    cellDiv[2].classList.add('won');
+} else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+    handleWin(middleLeft);
+    cellDiv[3].classList.add('won');
+    cellDiv[4].classList.add('won');
+    cellDiv[5].classList.add('won');
+} else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+    
+}
+}
+}
+
+
 
     }
 
